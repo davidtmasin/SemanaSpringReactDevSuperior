c9ae587afe6435b2c638fd9efdab6fa98620866d
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-11-07 10:40:30 -0300

    Starting my Project - Add back and front folders

---
+++ backend/src/main/java/com/semanaspringreact/devsuperior/dsvendas/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.semanaspringreact.devsuperior.dsvendas.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/semanaspringreact/devsuperior/dsvendas/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semanaspringreact.devsuperior.dsvendas.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
