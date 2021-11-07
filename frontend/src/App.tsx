7ad5f67418bf075daacdc7358242955b1033d1cf
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-11-07 11:38:34 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from 'components/DataTable'
+import Footer from 'components/Footer'
+import NavBar from 'components/NavBar'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Semana Spring React - Dev Superior</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Semana Spring React - Dev Superior</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,100 @@
+const DataTable = () => {
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Vendedor</th>
+            <th>Clientes visitados</th>
+            <th>Negócios fechados</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Jailson Mendes</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Paulo Guina</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Nego Ban</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Mc Gorila</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Kid Bengala</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Careca da Brazzers</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Trigesa VIP</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Alexis Texas</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Mia Khalifa</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+
+          <tr>
+            <td>22/04/2021</td>
+            <td>Mc BinLaden</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default DataTable
