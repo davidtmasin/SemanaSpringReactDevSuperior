5d303e7d7e59bb21ef4a0da03cdb70e8a9330fc9
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-11-07 18:51:46 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,11 +25,6 @@ const DonutChart = () => {
     })
   }, [])
 
-  // const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ['Jubileu', 'Jailson Mendes', 'Chewbacca', 'Xablau', 'Paulo Guina']
-  // }
-
   const options = {
     legend: {
       show: true
