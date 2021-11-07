7d737ab8106472573a8388639c5b3ed6fb945cf4
David Teixeira de Masin <davidteixeira.info@gmail.com>
2021-11-07 18:29:19 -0300

    First Request fixed

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -10,21 +10,16 @@ type ChartData = {
 }
 
 const DonutChart = () => {
-  const [chartData, setChartData] = useState<ChartData>({
-    labels: [],
-    series: []
+  let chartData: ChartData = { labels: [], series: [] }
+  axios.get(`${BASE_URL}/sales/amount-by-seller`).then(response => {
+    const data = response.data as SaleSum[]
+    const myLabels = data.map(x => x.sellerName)
+    const mySeries = data.map(x => x.sum)
+
+    chartData = { labels: myLabels, series: mySeries }
+    console.log(response.data)
   })
-  useEffect(() => {
-    axios.get(`${BASE_URL}+/sales/amount-by-seller`).then(response => {
-      const data = response.data as SaleSum[]
-      const myLabels = data.map(x => x.sellerName)
-      const mySeries = data.map(x => x.sum)
-      setChartData({
-        labels: myLabels,
-        series: mySeries
-      })
-    })
-  }, [])
+
   // const mockData = {
   //   series: [477138, 499928, 444867, 220426, 473088],
   //   labels: ['Jubileu', 'Jailson Mendes', 'Chewbacca', 'Xablau', 'Paulo Guina']
